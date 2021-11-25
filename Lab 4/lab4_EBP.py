7669bf508df59b6b1d14b9f995f22f6e2c7d4815
William Lubiantoro <william.lubiantoro@gmail.com>
2021-11-25 17:24:27 +0000

    Started Lab4 with a python script

---
+++ Lab 4/lab4_EBP.py
@@ -0,0 +1,18 @@
+import sys
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def sigmoid():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        sys.exit()
