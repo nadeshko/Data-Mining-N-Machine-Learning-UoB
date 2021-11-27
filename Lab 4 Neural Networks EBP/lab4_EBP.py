d4e71d0d1f9c487f58fcacbf922e3d64f44bf0f9
William Lubiantoro <william.lubiantoro@gmail.com>
2021-11-27 11:27:30 +0000

    Text Retrieval Report

--- Lab 4 Neural Networks EBP/lab4_EBP.py
+++ Lab 4 Neural Networks EBP/lab4_EBP.py
@@ -19,22 +19,32 @@ def sigmoid(x):
 
 def main():
     lr = 0.1
+    Delta_W1 = np.zeros((2,4))
+    Delta_W2 = np.zeros((4,2))
 
     input, target, W_1, W_2 = initialize_network()
-    for epoch in range(1001):
+    for epoch in range(1000):
         for i in range(4):
             y_in  = np.matmul(W_1, input[i])
             y_out = sigmoid(y_in)
             o_in  = np.matmul(W_2, y_out)
             o_out = sigmoid(o_in)
             Error = (1 / 2) * ((np.power((o_out - target[i]), 2)).sum())
-
             deltaj_out = (o_out - target[i]) * o_out * (1 - o_out)
-            deltaj_H   = (np.matmul(W_2.T, deltaj_out)).sum() * o_out * (1 - o_out)
-            delta_W1   = -(lr) * deltaj_H * input[i]
-            W_1 = W_1 + delta_W1
-        if epoch % 100 == 0:
-            print('epoch: ',epoch, 'error: ', Error)
+            print(Delta_W2)
+            print(deltaj_out.shape)
+            print(np.array(y_out))
+            #elta_W2   = Delta_W2 + ( -lr * (np.dot(np.array(y_out).T, deltaj_out)))
+
+            #deltaj_H   = (np.matmul(W_2.T, deltaj_out)) * y_out * (1 - y_out)
+            #Delta_W1   = Delta_W1 + ( -lr * (np.matmul(input[i], deltaj_H)))
+
+        #if epoch % 100 == 0:
+            #print('epoch: ',epoch, 'error: ', Error)
+        #W_2 = W_2 + Delta_W2/4
+        #W_1 = W_1 + Delta_W1/4
+        #Delta_W2 = 0
+        #Delta_W1 = 0
 
 if __name__ == '__main__':
     try:
